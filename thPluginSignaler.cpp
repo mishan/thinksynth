3a4e1968b3979a9cf861892fdb55742b91def1c5
misha <misha@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-01-04 23:48:47 +0000

    finished signaler code
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@44 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- thPluginSignaler.cpp
+++ thPluginSignaler.cpp
@@ -38,3 +38,36 @@ int thPluginSignaler::HookSignal (thPluginSignal *signal)
 
 	return 0;
 }
+
+void thPluginSignaler::UnhookSignal (thPluginSignal *signal)
+{
+	thListNode *node;
+	
+	if(!signal) {
+		return;
+	}
+
+	for(node = plugSignals[signal->sigNum]->GetHead(); node; 
+		node = node->prev) {
+		if(node->data == signal) {
+			plugSignals[signal->sigNum]->Remove(node);
+		}
+	}
+}
+
+int thPluginSignaler::Fire (int sig, void *a, void *b, void *c, void *d,
+							void *e, char f)
+{
+	thListNode *node;
+	int flag = 0;
+
+	for(node = plugSignals[sig]->GetHead(); node; node = node->prev) {
+		thPluginSignal *signal = (thPluginSignal *)node->data;
+
+		if(signal && signal->callback) {
+			flag = signal->callback (a, b, c, d, e, f);
+		}
+	}
+
+	return flag;
+}
