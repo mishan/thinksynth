3434d751fad9ed048e2ae76bce01267a4b00c24c
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-04-22 02:20:21 +0000

    added plugin dir stuff
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@143 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- dsp/test.dsp
+++ dsp/test.dsp
@@ -1,11 +1,11 @@
 name "test";
 
-node test1 foo::foo {
+node test1 test {
    foo = 0;
    bar = test2->foo;
    point = test2->bar;
 };
-node test2 foo::bar {
+node test2 test {
    foo = 12;
    bar = test1->bar;
 };
--- plugins/test.cpp
+++ plugins/test.cpp
@@ -1,61 +1,44 @@
 #define USE_PLUGIN
 
 #include <stdio.h>
-#include "plugin.h"
+#include <stdlib.h>
+#include <string.h>
+
+#include "thArg.h"
+#include "thList.h"
+#include "thBSTree.h"
+#include "thPlugin.h"
+#include "thPluginManager.h"
+#include "thNode.h"
+#include "thMod.h"
+#include "thSynth.h"
+
 // XXX   #include "thnodes.h"
 
 char	*name = "test";
 char	*desc = "Test Plugin";
 
-int	init_plug ( void *a, void *b);
-int	plugin_func ( void *a, void *b, unsigned int c);
-
-struct	xp_signal init_sig;
-struct	xp_signal func_sig;
+int	init_plug (int version, thPlugin *plugin);
+int	plugin_func (void *node, void *mod, unsigned int windowlen);
 
 int	module_init (int ver, class thPlugin plugin)
 {
-	/* This check *MUST* be done first */
-	if (ver != MODULE_IFACE_VER)
-		return 1;
-	
-	if (module_find (name) != NULL) {
-		/* We are already loaded */
-		printf("Module already loaded\n");
-		return 1;
-	}
-	
-	mod->name = name;
-	mod->desc = desc;
-		
-	init_sig.signal = 0;
-	init_sig.callback = XP_CALLBACK(init_plug);
-	init_sig.naddr = 0;
-	init_sig.mod = mod;
-
-	func_sig.callback = XP_CALLBACK(plugin_func);
-        func_sig.naddr = 0;
-        func_sig.mod = mod;
-
-	hook_signal(&init_sig);
-
-	return 0;
+  /* We should do version checks and stuff here */
+  return 0;
 }
 
 void module_cleanup (struct module *mod)
 {
-	printf("Sample module unloading\n");
+  printf("Sample module unloading\n");
 }
 
-int init_plug ( void *a, void *b, void *c, void *d, void *e, char f)
+int init_plug (int version, thPlugin *plugin)
 {
-	printf("mixer::add loaded on signal %i\n", (int)a);
-        func_sig.signal = (int)a;
-	hook_signal(&func_sig);
-	return 1;
+  printf("test plugin loaded\n");
+  return 0;
 }
 
-int plugin_func ( void *a, void *b, void *c, void *d, void *e, char f)
+int plugin_func (void *node, void *mod, unsigned int windowlen)
 {
   printf("TEST!!\n");
   return 0;
