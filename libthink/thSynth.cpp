0d492da3d2b0adfec24ff6f61b8d922165c95aba
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-04-24 22:34:21 +0000

    bye bye, thSynth::BuildSynthTree etc
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@214 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- libthink/thSynth.cpp
+++ libthink/thSynth.cpp
@@ -62,57 +62,6 @@ void thSynth::ListMods(void)
 	modlist.PrintTree();
 }
 
-void thSynth::BuildSynthTree(const char *modname)
-{
-	thMod *mod = FindMod(modname);
-	thListNode *listnode;
-	thArgValue *data;
-	thNode *ionode = mod->GetIONode();
-
-	ionode->SetRecalc(true);  /* We dont want to recalc the root if something
-				points here */
-
-	if(ionode->GetArgList()) {
-		for(listnode = ((thList *)ionode->GetArgList())->GetHead() ; listnode ; listnode = listnode->prev) {
-			data = (thArgValue *)((thArg *)listnode->data)->GetArg();
-			if(data->argType == ARG_POINTER) {
-				BuildSynthTreeHelper(mod, ionode, data->argPointNode);
-			}
-		}
-	}
-}
-
-int thSynth::BuildSynthTreeHelper(thMod *mod, thNode *parent, char *nodename)
-{
-	thListNode *listnode;
-	thArgValue *data;
-	thNode *currentnode = mod->FindNode(nodename);
-	thNode *node;
-
-	if(currentnode->GetRecalc() == true) {
-		return(1);  /* This node has already been processed */
-	}
-
-	currentnode->SetRecalc(true);  /* This node has now been marked as processed */
-
-	parent->AddChild(currentnode);
-	currentnode->AddParent(parent);
-	printf("Added Child %s to %s\n", currentnode->GetName(), parent->GetName());
-
-	if(currentnode->GetArgList()) {
-		for(listnode = ((thList *)currentnode->GetArgList())->GetHead(); listnode ; listnode = listnode->prev) {
-			data = (thArgValue *)((thArg *)listnode->data)->GetArg();
-			if(data->argType == ARG_POINTER) {
-				node = mod->FindNode(data->argPointNode);
-				if(node->GetRecalc() == false) {  /* Dont do the same node over and over */
-					BuildSynthTreeHelper(mod, currentnode, data->argPointNode);
-				}
-			}
-		}
-	}
-	return(0);
-}
-
 const thPluginManager *thSynth::GetPluginManager(void)
 {
 	return &pluginmanager;
