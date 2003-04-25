00dc248078845a3d9ccc3b9c2e8d84e32a70a486
joshk <joshk@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-04-25 07:03:40 +0000

    getopt support which doesn't work
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@220 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "think.h"
 
@@ -29,27 +30,55 @@
 
 int main (int argc, char *argv[])
 {
-  thMod *newmod;
+	thMod *newmod;
+	int havearg;
+	char *filename;
 
-  if(argc != 2) {
-    printf("Usage: %s <filename>\n", argv[0]);
-    exit(1);
-  }
+	if (argc < 2) /* Not enough parameters */
+	{
+		printf ("error: not enough parameters\n");
+syntax:
+		printf("Usage: %s [options] dsp-file\n", argv[0]);
+		exit(1);
+	}
   
-  Synth.LoadMod(argv[1]);
-  Synth.ListMods();
-  ((thMod *)Synth.FindMod("static"))->BuildSynthTree();
-  /* ((thMod *)Synth->FindMod("test"))->Process(); */
+	while ((havearg = getopt (argc, argv, "h") != -1)) {
+		switch (havearg) {
+			case 'h':
+				printf (PACKAGE VERSION " by Misha Nasledov, Leif M. Ames, Aaron Lehmann and Joshua Kwan\n");
+				/* TODO: insert some helpful text here */
+				goto syntax;
+				break;
+
+			default:
+				if (optind != argc) {
+					printf ("error: unrecognized parameter\n");
+					goto syntax;
+				}
+		}
+	}
+
+	if (optind == argc) {
+		printf ("error: no input file\n");
+		goto syntax;
+	}
+	else {
+		filename = strdup (argv[optind]);
+	}
+
+	Synth.LoadMod(filename);
+	Synth.ListMods();
+	((thMod *)Synth.FindMod("static"))->BuildSynthTree();
+	/* ((thMod *)Synth->FindMod("test"))->Process(); */
   
+	((thMod *)Synth.FindMod("static"))->BuildSynthTree();
+	Synth.AddChannel("chan1", "static");
+	Synth.AddNote("chan1", 20, 100);
 
-  ((thMod *)Synth.FindMod("static"))->BuildSynthTree();
-  Synth.AddChannel("chan1", "static");
-  Synth.AddNote("chan1", 20, 100);
+	newmod = ((thMod *)Synth.FindMod("static"))->Copy();
+	newmod->BuildSynthTree();
+	newmod->Process(newmod, 1024);
 
-  newmod = ((thMod *)Synth.FindMod("static"))->Copy();
-  newmod->BuildSynthTree();
-  newmod->Process(newmod, 1024);
-
-  /*  printf("  = %f\n", ((thArgValue *)((thMod *)Synth.FindMod("static"))->GetArg("static", "out"))->argValues[0]); */
-  printf("  = %f\n", ((thArgValue *)newmod->GetArg("ionode", "out"))->argValues[0]);
+	/*  printf("  = %f\n", ((thArgValue *)((thMod *)Synth.FindMod("static"))->GetArg("static", "out"))->argValues[0]); */
+	printf("  = %f\n", ((thArgValue *)newmod->GetArg("ionode", "out"))->argValues[0]);
 }
