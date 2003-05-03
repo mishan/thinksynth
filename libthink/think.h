a3f059cf8931c53a3493830d869ac46ec2c35591
joshk <joshk@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-05-03 03:50:55 +0000

    comment out the think.h variadic macro that busts up the g++-2.95 build
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@409 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- libthink/think.h
+++ libthink/think.h
@@ -1,4 +1,4 @@
-/* $Id: think.h,v 1.10 2003/04/29 01:10:25 ink Exp $ */
+/* $Id: think.h,v 1.11 2003/05/03 03:50:55 joshk Exp $ */
 
 #ifndef THINK_H
 #define THINK_H
@@ -18,10 +18,15 @@
 #define OUTPUTPREFIX "out"
 
 /* Handy debug function */
+
+#if 0
+
 #ifdef USE_DEBUG
 #define debug(...) printf("%s:%d: ", __FILE__, __LINE__);printf(...);printf("\n");
 #else
 #define debug(...) ;
 #endif /* USE_DEBUG */
+
+#endif /* above code is majorly owned */
 
 #endif /* THINK_H */
