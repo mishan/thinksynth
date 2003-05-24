2fc0d687b7feade1a38be73917bfce95c7fa0447
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-05-24 08:25:30 +0000

    optimized mixers
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@638 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- plugins/mixer/fade.cpp
+++ plugins/mixer/fade.cpp
@@ -1,4 +1,4 @@
-/* $Id: fade.cpp,v 1.2 2003/05/17 15:27:30 ink Exp $ */
+/* $Id: fade.cpp,v 1.3 2003/05/24 08:25:30 ink Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,16 +42,24 @@ int module_callback (thNode *node, thMod *mod, unsigned int windowlen)
 	float *out;
 	thArgValue *in_0, *in_1, *in_fade;
 	thArgValue *out_arg;
-	unsigned int i;
-
-	out_arg = (thArgValue *)mod->GetArg(node, "out");
-	out = out_arg->allocate(windowlen);
+	unsigned int i, largest;
 
 	in_0 = (thArgValue *)mod->GetArg(node, "in0");
 	in_1 = (thArgValue *)mod->GetArg(node, "in1");
 	in_fade = (thArgValue *)mod->GetArg(node, "fade");
 
-	for(i=0;i<windowlen;i++) {
+	largest = in_0->argNum;
+	if((unsigned int)in_1->argNum > largest) {
+		largest = in_1->argNum;
+	}
+	if((unsigned int)in_fade->argNum > largest) {
+		largest = in_fade->argNum;
+	}
+
+	out_arg = (thArgValue *)mod->GetArg(node, "out");
+	out = out_arg->allocate(largest);
+
+	for(i=0;i<largest;i++) {
 		out[i] = ((*in_0)[i] * (1-(*in_fade)[i])) + ((*in_1)[i] * (*in_fade)[i]);
 	}
 
