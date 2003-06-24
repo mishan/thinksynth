8c17cc74878c6e70d53529b4a5dfb96c4a2852cf
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-06-24 21:39:01 +0000

    Added a few logic functions
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@738 7344778b-dfeb-0310-9d57-fd9178b6fdc4

---
+++ plugins/logic/and.cpp
@@ -0,0 +1,61 @@
+/* $Id: and.cpp,v 1.1 2003/06/24 21:39:01 ink Exp $ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "think.h"
+
+#include "thArg.h"
+#include "thPlugin.h"
+#include "thPluginManager.h"
+#include "thNode.h"
+#include "thMod.h"
+#include "thSynth.h"
+
+char		*desc = "Logical And";
+thPluginState	mystate = thPassive;
+
+extern "C" int	module_init (thPlugin *plugin);
+extern "C" int	module_callback (thNode *node, thMod *mod, unsigned int windowlen);
+extern "C" void module_cleanup (struct module *mod);
+
+void module_cleanup (struct module *mod)
+{
+	printf("Logical And plugin unloading\n");
+}
+
+int module_init (thPlugin *plugin)
+{
+	printf("Logical And plugin loaded\n");
+
+	plugin->SetDesc (desc);
+	plugin->SetState (mystate);
+
+	return 0;
+}
+
+int module_callback (thNode *node, thMod *mod, unsigned int windowlen)
+{
+	float *out;
+	thArg *in_0, *in_1;
+	thArg *out_arg;
+	unsigned int i;
+
+	in_0 = mod->GetArg(node, "in0");
+	in_1 = mod->GetArg(node, "in1");
+
+	out_arg = mod->GetArg(node, "out");
+	out = out_arg->allocate(windowlen);
+
+	for(i=0;i<windowlen;i++) {
+		if((*in_0)[i] > 0 && (*in_1)[i] > 0) {
+			out[i] = 1;
+		} else {
+			out[i] = 0;
+		}
+	}
+
+	return 0;
+}
+
---
+++ plugins/logic/not.cpp
@@ -0,0 +1,60 @@
+/* $Id: not.cpp,v 1.1 2003/06/24 21:39:01 ink Exp $ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "think.h"
+
+#include "thArg.h"
+#include "thPlugin.h"
+#include "thPluginManager.h"
+#include "thNode.h"
+#include "thMod.h"
+#include "thSynth.h"
+
+char		*desc = "Logical Not";
+thPluginState	mystate = thPassive;
+
+extern "C" int	module_init (thPlugin *plugin);
+extern "C" int	module_callback (thNode *node, thMod *mod, unsigned int windowlen);
+extern "C" void module_cleanup (struct module *mod);
+
+void module_cleanup (struct module *mod)
+{
+	printf("Logical Not plugin unloading\n");
+}
+
+int module_init (thPlugin *plugin)
+{
+	printf("Logical Not plugin loaded\n");
+
+	plugin->SetDesc (desc);
+	plugin->SetState (mystate);
+
+	return 0;
+}
+
+int module_callback (thNode *node, thMod *mod, unsigned int windowlen)
+{
+	float *out;
+	thArg *in;
+	thArg *out_arg;
+	unsigned int i;
+
+	in = mod->GetArg(node, "in");
+
+	out_arg = mod->GetArg(node, "out");
+	out = out_arg->allocate(windowlen);
+
+	for(i=0;i<windowlen;i++) {
+		if((*in)[i] > 0) {
+			out[i] = 0;
+		} else {
+			out[i] = 1;
+		}
+	}
+
+	return 0;
+}
+
