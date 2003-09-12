0302effb7d0c0bf00946f6fe8da293264a08acb8
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-09-12 12:48:38 +0000

    useful for comb filters
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@773 7344778b-dfeb-0310-9d57-fd9178b6fdc4

---
+++ plugins/misc/freq2samples.cpp
@@ -0,0 +1,59 @@
+/* $Id: freq2samples.cpp,v 1.1 2003/09/12 12:48:38 ink Exp $ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "think.h"
+
+#include "thArg.h"
+#include "thPlugin.h"
+#include "thPluginManager.h"
+#include "thNode.h"
+#include "thMod.h"
+#include "thSynth.h"
+
+char		*desc = "Converts a frequency to wavelength in samples";
+thPluginState	mystate = thPassive;
+
+extern "C" int	module_init (thPlugin *plugin);
+extern "C" int	module_callback (thNode *node, thMod *mod, unsigned int windowlen);
+extern "C" void module_cleanup (struct module *mod);
+
+void module_cleanup (struct module *mod)
+{
+	printf("Freq2Samples plugin unloading\n");
+}
+
+int module_init (thPlugin *plugin)
+{
+	printf("Freq2Samples plugin loaded\n");
+
+	plugin->SetDesc (desc);
+	plugin->SetState (mystate);
+
+	return 0;
+}
+
+int module_callback (thNode *node, thMod *mod, unsigned int windowlen)
+{
+	float *out;
+	thArg *in_freq;
+	thArg *out_arg;
+	unsigned int i, argnum;
+
+	in_freq = mod->GetArg(node, "freq");
+
+	out_arg = mod->GetArg(node, "out");
+	argnum = (unsigned int) in_freq->argNum;
+	out = out_arg->allocate(argnum);
+
+	for(i=0;i<argnum;i++) {
+	  out[i] = (1/(*in_freq)[i])*TH_SAMPLE;
+	}
+
+/*	node->SetArg("out", out, windowlen); */
+	return 0;
+}
+
