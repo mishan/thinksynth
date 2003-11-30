e2fe5e520baf840dd3eebb58211960d5bed076e4
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-11-30 14:03:28 +0000

    added envelope follower
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@840 7344778b-dfeb-0310-9d57-fd9178b6fdc4

---
+++ plugins/env/follower.cpp
@@ -0,0 +1,83 @@
+/* $Id: follower.cpp,v 1.1 2003/11/30 14:03:28 ink Exp $ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "think.h"
+
+#include "thArg.h"
+#include "thPlugin.h"
+#include "thPluginManager.h"
+#include "thNode.h"
+#include "thMod.h"
+#include "thSynth.h"
+
+char		*desc = "Follows the envelope of the input";
+thPluginState	mystate = thActive;
+
+extern "C" int	module_init (thPlugin *plugin);
+extern "C" int	module_callback (thNode *node, thMod *mod, unsigned int windowlen);
+extern "C" void module_cleanup (struct module *mod);
+
+void module_cleanup (struct module *mod)
+{
+	printf("Envelope Follower plugin unloading\n");
+}
+
+int module_init (thPlugin *plugin)
+{
+	printf("Envelope Follower plugin loaded\n");
+
+	plugin->SetDesc (desc);
+	plugin->SetState (mystate);
+
+	return 0;
+}
+
+int module_callback (thNode *node, thMod *mod, unsigned int windowlen)
+{
+	float *out, *out_last;
+	thArg *in_arg, *in_falloff;
+	thArg *out_arg;
+	thArg *inout_last;
+	unsigned int i;
+	float last, absolute;
+
+	out_arg = mod->GetArg(node, "out");
+	inout_last = mod->GetArg(node, "last");
+
+	last = (*inout_last)[0];
+
+	out_last = inout_last->Allocate(1);
+
+	out = out_arg->Allocate(windowlen);
+
+	in_arg = mod->GetArg(node, "in");
+	in_falloff = mod->GetArg(node, "falloff");
+
+
+	for(i = 0; i < windowlen; i++)
+	{
+		absolute = fabs((*in_arg)[i]);
+
+		if(absolute >= last)
+		{
+			last = absolute;
+		}
+		else
+		{
+			last -= (*in_falloff)[i];
+		}
+
+		out[i] = last;
+	}
+
+	out_last[0] = last;
+
+	
+
+	return 0;
+}
+
