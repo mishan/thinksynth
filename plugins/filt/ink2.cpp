2ce5146d3b62d99436d9e734481c52fcde49417b
ink <ink@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2003-12-02 06:22:41 +0000

    leet new filter!
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@843 7344778b-dfeb-0310-9d57-fd9178b6fdc4

---
+++ plugins/filt/ink2.cpp
@@ -0,0 +1,82 @@
+/* $Id: ink2.cpp,v 1.1 2003/12/02 06:22:41 ink Exp $ */
+
+/* Written by Leif Ames <ink@bespni.org>
+   Algorithm taken from musicdsp.org posted by Paul Kellett */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "think.h"
+
+#include "thArg.h"
+#include "thPlugin.h"
+#include "thPluginManager.h"
+#include "thNode.h"
+#include "thMod.h"
+#include "thSynth.h"
+
+#define SIGN(x) (x < 0) ? -1 : 1
+
+char		*desc = "Resonant 1-pole LPF";
+thPluginState	mystate = thActive;
+
+extern "C" int	module_init (thPlugin *plugin);
+extern "C" int	module_callback (thNode *node, thMod *mod, unsigned int windowlen);
+extern "C" void module_cleanup (struct module *mod);
+
+void module_cleanup (struct module *mod)
+{
+	printf("ResLPF plugin unloading\n");
+}
+
+int module_init (thPlugin *plugin)
+{
+	printf("ResLPF plugin loaded\n");
+
+	plugin->SetDesc (desc);
+	plugin->SetState (mystate);
+
+	return 0;
+}
+
+int module_callback (thNode *node, thMod *mod, unsigned int windowlen)
+{
+	float *out, *buffer;
+	thArg *in_arg, *in_cutoff, *in_res;
+	thArg *out_arg;
+	thArg *inout_buffer;
+	float buf0, buf1, in;
+	float f, q;  /* cutoff, res */
+	unsigned int i;
+
+	out_arg = mod->GetArg(node, "out");
+	out = out_arg->Allocate(windowlen);
+
+	inout_buffer = mod->GetArg(node, "buffer");
+	buf0 = (*inout_buffer)[0];
+	buf1 = (*inout_buffer)[1];
+	buffer = inout_buffer->Allocate(2);
+
+	in_arg = mod->GetArg(node, "in");
+	in_cutoff = mod->GetArg(node, "cutoff");
+	in_res = mod->GetArg(node, "res");
+
+	for(i=0;i<windowlen;i++) {
+		f = (*in_cutoff)[i];
+		q = (*in_res)[i];
+		in = (*in_arg)[i];
+
+		buf0 *= 1 - f;
+		buf0 += (buf1 - in) * f;
+		buf1 *= 1 - (f * q);
+		buf1 += (in - buf0) * q;
+
+		out[i] = (buf0 - buf1); // - buf1);
+	}
+
+	buffer[0] = buf0;
+	buffer[1] = buf1;
+	return 0;
+}
+
