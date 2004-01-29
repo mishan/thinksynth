7d56f960f4073652f097f604f3d6a1f6bebae30f
misha <misha@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2004-01-29 06:12:23 +0000

    fixed build error
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@877 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- libthink/thALSAAudio.cpp
+++ libthink/thALSAAudio.cpp
@@ -1,4 +1,4 @@
-/* $Id: thALSAAudio.cpp,v 1.5 2004/01/29 05:56:27 ink Exp $ */
+/* $Id: thALSAAudio.cpp,v 1.6 2004/01/29 06:12:23 misha Exp $ */
 
 #include "config.h"
 
@@ -54,17 +54,22 @@ void thALSAAudio::SetFormat (const thAudioFmt *afmt)
 										SND_PCM_FORMAT_S16);
 		   break;
    }
-    snd_pcm_hw_params_set_rate_near(play_handle, hw_params, ifmt.samples, 0);
-    snd_pcm_hw_params_set_channels(play_handle, hw_params, ifmt.channels);
-    snd_pcm_hw_params_set_periods(play_handle, hw_params, 2, 0);
-    snd_pcm_hw_params_set_period_size(play_handle, hw_params, ifmt.period * 1.75, 0); /* where the buffer is actually set. */
+   
+   snd_pcm_hw_params_set_rate_near(play_handle, hw_params, (unsigned int *)&ifmt.samples, NULL);
+   snd_pcm_hw_params_set_channels(play_handle, hw_params, ifmt.channels);
+   snd_pcm_hw_params_set_periods(play_handle, hw_params, 2, 0);
 
+   /* where the buffer is actually set */
+   /* XXX: what is this constant 1.75?? */
+   snd_pcm_hw_params_set_period_size(play_handle, hw_params,
+									 (int)(ifmt.period * 1.75), 0);
+   
 //	snd_pcm_hw_params_set_buffer_time(play_handle, hw_params, 1000, 0);
-    snd_pcm_hw_params(play_handle, hw_params);
-    snd_pcm_sw_params_alloca(&sw_params);
-    snd_pcm_sw_params_current(play_handle, sw_params);
-    snd_pcm_sw_params_set_avail_min(play_handle, sw_params, ifmt.period);
-    snd_pcm_sw_params(play_handle, sw_params);
+   snd_pcm_hw_params(play_handle, hw_params);
+   snd_pcm_sw_params_alloca(&sw_params);
+   snd_pcm_sw_params_current(play_handle, sw_params);
+   snd_pcm_sw_params_set_avail_min(play_handle, sw_params, ifmt.period);
+   snd_pcm_sw_params(play_handle, sw_params);
 }
 
 int thALSAAudio::Read (void *outbuf, int len)
@@ -77,7 +82,7 @@ int thALSAAudio::Write (float *inbuf, int len)
 	int i ,w = 0;
 	int chans = ofmt.channels;
 	int bytes = ofmt.bits / 8;
-	int samplelen = bytes*chans;
+//	int samplelen = bytes*chans;
 
 		/* malloc an appropriate buffer it would be *bad* if the length of the 
 	   buffer passed in were to increase so don't do that (brandon) */
