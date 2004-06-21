186a95e02a7ee0308b4ed37f695523c41bab1b57
joshk <joshk@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2004-06-21 04:54:40 +0000

    define RTLD_LAZY and RTLD_NOW if necessary
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@1190 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- libthink/nsmodule_dl.h
+++ libthink/nsmodule_dl.h
@@ -1,9 +1,18 @@
 /* dl(3) wrappers  for OS X
- * $Id: nsmodule_dl.h,v 1.1 2003/12/21 05:57:48 joshk Exp $
+ * $Id: nsmodule_dl.h,v 1.2 2004/06/21 04:54:40 joshk Exp $
  */
 
 #ifndef INCLUDED_nsmodule_dl_h
 #define INCLUDED_nsmodule_dl_h
+
+#include <mach-o/dyld.h>
+
+#ifndef RTLD_LAZY
+#  define RTLD_LAZY 2185
+#endif
+#ifndef RTLD_NOW
+#  define RTLD_NOW RTLD_LAZY
+#endif
 
 char *dlerror(void);
 void *dlopen(char *filename, int unused);
