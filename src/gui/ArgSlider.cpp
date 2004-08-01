84e3249818d17c9a64ecb43e276c637848d2d57e
misha <misha@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2004-08-01 09:57:56 +0000

    added ArgSlider class
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@1220 7344778b-dfeb-0310-9d57-fd9178b6fdc4

---
+++ src/gui/ArgSlider.cpp
@@ -0,0 +1,25 @@
+#include "config.h"
+
+#include <gtkmm.h>
+
+#include "think.h"
+
+#include "ArgSlider.h"
+
+ArgSlider::ArgSlider (thArg *arg)
+	: Gtk::HScale (arg->argMin, arg->argMax, .01)
+{
+	arg_ = arg;
+	set_value (arg_->argValues[0]);
+	set_draw_value();
+	set_value_pos(Gtk::POS_TOP);
+}
+
+ArgSlider::~ArgSlider ()
+{
+}
+
+void ArgSlider::on_value_changed ()
+{
+	arg_->argValues[0] = get_value();
+}
---
+++ src/gui/ArgSlider.h
@@ -0,0 +1,16 @@
+#ifndef ARG_SLIDER_H
+#define ARG_SLIDER_H
+
+class ArgSlider : public Gtk::HScale
+{
+public:
+	ArgSlider(thArg *arg);
+	~ArgSlider (void);
+
+protected:
+	virtual void on_value_changed ();
+
+	thArg *arg_;
+};
+
+#endif /* ARG_SLIDER_H */
