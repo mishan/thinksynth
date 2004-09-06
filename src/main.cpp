75735e8bf9dd60d694c9e4d3bc0ba3eb898a7506
misha <misha@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2004-09-06 23:57:33 +0000

    cleaned up some code (test commit)
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@1291 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- src/main.cpp
+++ src/main.cpp
@@ -297,9 +297,8 @@ int main (int argc, char *argv[])
 	process->connect(SigC::slot(process_synth));
 
 	signal(SIGUSR1, (sighandler_t)cleanup);
-        signal(SIGINT, (sighandler_t)cleanup);
+	signal(SIGINT, (sighandler_t)cleanup);
 
-//	read_prefs (Synth);
 	prefs->Load();
 
 	/* create UI thread */
