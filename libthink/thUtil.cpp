27a8a5a22b2ff15d3f68aacf737478f5db622461
joshk <joshk@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2005-02-21 03:09:41 +0000

    replace
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@1612 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- libthink/thUtil.cpp
+++ libthink/thUtil.cpp
@@ -55,72 +55,22 @@ char* thUtil::basename(char *path)
 	return (s);
 }
 
-/* dirname - stolen from glibc in part. GPL 2. */
+/* dirname - stolen from old Busybox. */
 
 char *
 thUtil::dirname (char *path_)
 {
-  /* Deviation from standard: allocate a new buffer to hold the string. */
-  char *path = strdup(path_);
-  static const char dot[] = ".";
-  char *last_slash;
+	/* Still make a copy */
+	char *str = strdup(path_) + strlen(path_) - 1, *s = NULL;
+	
+	while (str && *str == '/') {
+		*str-- = '\0';
+	}
+	
+	s = strrchr(str, '/');
 
-  /* Find last '/'.  */
-  last_slash = path != NULL ? strrchr (path, '/') : NULL;
+	if (s && *s)
+		*s = '\0';
 
-  if (last_slash != NULL && last_slash != path && last_slash[1] == '\0')
-    {
-      /* Determine whether all remaining characters are slashes.  */
-      char *runp;
-
-      for (runp = last_slash; runp != path; --runp)
-        if (runp[-1] != '/')
-          break;
-
-      /* The '/' is the last character, we have to look further.  */
-      if (runp != path)
-        last_slash = (char*) memrchr ((void*)path, '/', runp - path);
-    }
-
-  if (last_slash != NULL)
-    {
-      /* Determine whether all remaining characters are slashes.  */
-      char *runp;
-
-      for (runp = last_slash; runp != path; --runp)
-        if (runp[-1] != '/')
-          break;
-
-      /* Terminate the path.  */
-      if (runp == path)
-        {
-          /* The last slash is the first character in the string.  We have to
-             return "/".  As a special case we have to return "//" if there
-             are exactly two slashes at the beginning of the string.  See
-             XBD 4.10 Path Name Resolution for more information.  */
-          if (last_slash == path + 1)
-            ++last_slash;
-          else
-            last_slash = path + 1;
-        }
-      else
-        last_slash = runp;
-
-      last_slash[0] = '\0';
-    }
-  else
-    /* This assignment is ill-designed but the XPG specs require to
-       return a string containing "." in any case no directory part is
-       found and so a static and constant string is required.  */
-    path = (char *) dot;
-
-  if (path != NULL)
-  {
-    /* Save space; rval is somewhat smaller than path*/
-    char* rval = strdup(path);
-    free(path);
-    return rval;
-  }
-
-  return path;
+	return s;
 }
