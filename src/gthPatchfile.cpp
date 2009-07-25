841be93db6ed1c72204390c8d022c91ba115fc7c
misha <misha@7344778b-dfeb-0310-9d57-fd9178b6fdc4>
2009-07-25 23:20:11 +0000

    * include string.h so these will compile on modern systems
    git-svn-id: svn+ssh://thinksynth.org/var/lib/svn/thinksynth/trunk@1629 7344778b-dfeb-0310-9d57-fd9178b6fdc4

--- src/gthPatchfile.cpp
+++ src/gthPatchfile.cpp
@@ -24,6 +24,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 #include "think.h"
 
--- src/gthPrefs.cpp
+++ src/gthPrefs.cpp
@@ -22,6 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 #include "think.h"
 
